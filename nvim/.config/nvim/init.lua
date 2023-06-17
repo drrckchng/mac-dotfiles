26fde7a2d204c704eef5244ba2b5376ff8e4e463
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-06-16 22:29:39 -0700

    Source separate nvim settings if vscode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  vim.cmd("source $HOME/.config/vscode/settings.vim")
+else
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
+end
